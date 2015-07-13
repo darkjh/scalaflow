2bcc6c91c2ac294ab93768eb074edf37df28b357
akhmed1 <akhmed.umyarov@gmail.com>
2015-07-12 19:06:50 -0500

    Fixed the dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,8 @@ object ScalaFlowBuild extends Build {
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-reflect" % "2.10.4",
       "com.twitter" %% "chill" % "0.5.1",
-      "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % "manual_build",
+      "org.joda" % "joda-convert" % "1.2",
+      "com.google.cloud.dataflow" % "google-cloud-dataflow-java-sdk-all" % "0.3.141216",
       // test dependencies
       "org.scalatest" %% "scalatest" % "2.2.1" % "test",
       "org.hamcrest" % "hamcrest-all" % "1.3" % "test",
